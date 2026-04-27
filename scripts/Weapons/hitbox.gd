380d8cb0b45aec18c60663e0b44deb1f460a9c03
keithfarnham <keithfarnham@users.noreply.github.com>
2026-04-27 16:59:08 -0700

    hitbox/hurtbox changes

--- scripts/Weapons/hitbox.gd
+++ scripts/Weapons/hitbox.gd
@@ -1,16 +1,27 @@
 extends Area2D
 
-@onready var collision = $hitboxCollision as CollisionShape2D
-@onready var timer = $hitTimer as Timer
+class_name Hitbox
 
+@onready var collision = $hitboxCollision as CollisionShape2D
+@onready var cooldown_timer = $hitTimer as Timer
+
+@export var hitbox_type : HitboxType
 @export var damage = 1
+
+enum HitboxType {
+	Single, # does a single hit of damage to target
+	Cooldown, # damage over time on a cooldown while colliding with target
+}
 
 func disable():
 	collision.set_deferred("disabled", true)
 
 func cooldown():
-	collision.set_deferred("disabled", true)
-	timer.start()
+	#collision.set_deferred("disabled", true)
+	cooldown_timer.start()
 
 func _on_hit_timer_timeout():
 	collision.set_deferred("disabled", false)
+
+func remove_cooldown():
+	cooldown_timer.stop()
--- scripts/Weapons/hurtbox.gd
+++ scripts/Weapons/hurtbox.gd
@@ -1,34 +1,35 @@
 extends Area2D
 
-@export_enum("Cooldown", "Damage", "Disable") var hurtBoxType = 0
+class_name Hurtbox
 
 @onready var collision = $hurtboxCollision as CollisionShape2D
 @onready var timer = $hurtTimer as Timer
 
 signal hurt(damage)
 
-var on_cooldown = {}
+var colliding_hitbox = []
+
+func _process(delta):
+	for hitbox in colliding_hitbox:
+		if hitbox.get("cooldown_timer").is_stopped():
+			hurt.emit(hitbox.get("damage"))
+			hitbox.get("cooldown_timer").start()
 
 func _on_area_entered(area):
 	if area.is_in_group("attack"):
 		assert(area.get("damage") != null, "[hurtbox] area %s does not have damage value" % [area.name])
-		match (hurtBoxType):
-			0: # Cooldown on a per weapon basis
-				if area in on_cooldown:
-					return
-				Log.print("[hurtbox] Cooldown - area %s colliding with %s." % [str(name), str(area.name)])
-				collision.set_deferred("disabled", true)
-				on_cooldown.append(area)
-				timer.start()
-			1: # Single Hit
-				#emit_signal("damage", area.get("damage"))
+		match area.hitbox_type:
+			Hitbox.HitboxType.Single:
 				Log.print("[hurtbox] Single Hit - area %s colliding with %s." % [str(name), str(area.name)])
-			2: # Disable
-				Log.print("[hurtbox] Disable - area %s colliding with %s." % [str(name), str(area.name)])
-				if area.has_method("cooldown"):
-					area.cooldown()
-		hurt.emit(area.get("damage"))
+				hurt.emit(area.get("damage"))
+			Hitbox.HitboxType.Cooldown:
+				Log.print("[hurtbox] Cooldown - area %s colliding with %s." % [str(name), str(area.name)])
+				colliding_hitbox.append(area)
+				#TODO dmg delayed to next update might be a problem, do stuff from _process()?
 
-func _on_hurt_timer_timeout():
-	collision.set_deferred("disabled", false)
-	on_cooldown.find()
+func _on_area_exited(area):
+	if colliding_hitbox.has(area):
+		var hitbox_id = colliding_hitbox.find(area)
+		colliding_hitbox[hitbox_id].remove_cooldown()
+		colliding_hitbox.remove_at(hitbox_id)
+		
